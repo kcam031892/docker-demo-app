5656358864a29635c9c30c9403b946a12296f6e0
kcam031892 <mack031892@gmail.com>
2021-03-01 17:05:36 +0800

    inital commit

--- src/App.test.js
+++ src/App.test.js
@@ -6,3 +6,8 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('it should display mack031892', () => {
+  render(<App />);
+  const nameElement = screen.getByText(/mack031892/i);
+  expect(nameElement).toBeInTheDocument();
+});
